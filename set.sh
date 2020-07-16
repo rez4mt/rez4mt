9cda60782c1516963272af3a4f7c7051cd19ee99
Reza Motlagh <13662664+rez4mt@users.noreply.github.com>
2020-07-16 15:52:58 +0300

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sudo apt install python3.8
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 10
 find . -type f | awk -F"/" '$NF~ /^\..*$/ {system("rm -f ~/"$NF" && ln -s $(pwd)/"$NF" ~/"$NF)}'
