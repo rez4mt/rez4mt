74b73e2c8b69d4ec52b3432572d1f092f6d91125
Reza Motlagh <13662664+rez4mt@users.noreply.github.com>
2020-07-16 15:55:34 +0300

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+#install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#to install python3 on System
 sudo apt install python3.8
+#set python3 and pip3 as python and pip
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 10
+sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 10
+#copy .files to home folder
 find . -type f | awk -F"/" '$NF~ /^\..*$/ {system("rm -f ~/"$NF" && ln -s $(pwd)/"$NF" ~/"$NF)}'
