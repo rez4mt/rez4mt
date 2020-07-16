1beb561d3eb6c1a1d3d8bf3c380d8acb633cb6b2
Reza Motlagh <13662664+rez4mt@users.noreply.github.com>
2020-07-16 16:25:12 +0300

    Update set.sh

--- set.sh
+++ set.sh
@@ -2,7 +2,7 @@
 #install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 #to install python3 on System
-sudo apt install python3.8 pip3
+sudo apt install python3.8 pip3 tmux
 #set python3 and pip3 as python and pip
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 10
 sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 10
