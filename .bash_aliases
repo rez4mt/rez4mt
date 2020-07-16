92b71ea717b411be4079a5b3f0548655717ddaaf
Ubuntu <ubuntu@ip-172-31-37-214.ec2.internal>
2020-07-16 13:23:05 +0000

    set and bash aliases fix.

--- .bash_aliases
+++ .bash_aliases
@@ -1,1 +1,7 @@
 alias tmux="TERM=screen-256color-bce tmux"
+
+export WORKON_HOME=$HOME/.virtualenvs
+export VIRTUALENVWRAPPER_PYTHON=$(which python)
+source /usr/local/bin/virtualenvwrapper.sh
+export VIRTUALENVWRAPPER_ENV_BIN_DIR=bin
+
--- set.sh
+++ set.sh
@@ -2,9 +2,12 @@
 #install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 #to install python3 on System
-sudo apt install python3.8
+sudo apt install python3.8 pip3
 #set python3 and pip3 as python and pip
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 10
 sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 10
+#install virtualenv/wrapper
+sudo pip install --no-cache-dir virtualenv
+sudo pip install --no-cache-dir virtualenvwrapper
 #copy .files to home folder
 find . -type f | awk -F"/" '$NF~ /^\..*$/ {system("rm -f ~/"$NF" && ln -s $(pwd)/"$NF" ~/"$NF)}'
