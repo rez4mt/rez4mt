2d2ec306a13a387e1fa1871ff044a5d97ff4fcc1
Reza Motlagh <13662664+rez4mt@users.noreply.github.com>
2020-07-16 15:44:31 +0300

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
- curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sudo apt install python3.8
 find . -type f | awk -F"/" '$NF~ /^\..*$/ {system("rm -f ~/"$NF" && ln -s $(pwd)/"$NF" ~/"$NF)}'
